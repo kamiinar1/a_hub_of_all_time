decbb963cb3fa5219315404000d89c4875f6c9eb
thunderfurry <91995526+thunderfurry@users.noreply.github.com>
2023-03-26 19:58:25 -0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -5,7 +5,7 @@ local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/shle
 local Window = Rayfield:CreateWindow({
    Name = "A hub of all time™",
    LoadingTitle = "Certainly a hub.",
-   LoadingSubtitle = "by Furri and Terio",
+   LoadingSubtitle = "by Furri",
    ConfigurationSaving = {
       Enabled = true,
       FolderName = nil, -- Create a custom folder for your hub/game
@@ -28,12 +28,11 @@ local Window = Rayfield:CreateWindow({
    }
 })
 
-local Tab = Window:CreateTab("General", 4483362458) -- Title, Image
+local CharTab = Window:CreateTab("Character", 4483362458) -- Title, Image
 
-local Label = Tab:CreateLabel("Character Modifiers")
-local Slider = Tab:CreateSlider({
+local Slider = CharTab:CreateSlider({
    Name = "Walkspeed",
-   Range = {0, 100},
+   Range = {0, 250},
    Increment = 2,
    Suffix = "Walkspeed",
    CurrentValue = 16,
@@ -43,9 +42,9 @@ local Slider = Tab:CreateSlider({
    end,
 })
 
-local Slider = Tab:CreateSlider({
+local Slider = CharTab:CreateSlider({
    Name = "Jump Power",
-   Range = {0, 100},
+   Range = {0, 1000},
    Increment = 2,
    Suffix = "Jump power",
    CurrentValue = 50,
@@ -56,9 +55,9 @@ local Slider = Tab:CreateSlider({
    end,
 })
 
-local Slider = Tab:CreateSlider({
+local Slider = CharTab:CreateSlider({
    Name = "FOV",
-   Range = {0, 100},
+   Range = {0, 120},
    Increment = 2,
    Suffix = "FOV",
    CurrentValue = 50,
@@ -68,15 +67,87 @@ local Slider = Tab:CreateSlider({
    end,
 })
 
-local Label = Tab:CreateLabel("Scripts")
-local Button = Tab:CreateButton({
+--SCRIPTS
+
+local ScriptsTab = Window:CreateTab("Scripts", 4483362458) -- Title, Image
+--FEFLIP
+local Button = ScriptsTab:CreateButton({
+   Name = "FeFlip",
+   Callback = function()
+ 	loadstring(game:HttpGet("https://pastebin.com/raw/crPcd0vq",true))()
+   end,
+})
+--AntiGrav
+local Button = ScriptsTab:CreateButton({
    Name = "Walk on walls",
    Callback = function()
-   loadstring(game:HttpGet("https://raw.githubusercontent.com/thunderfurry/a_hub_of_all_time/main/scripts/wallwalk.lua"))()
+ 	loadstring(game:HttpGet("https://raw.githubusercontent.com/thunderfurry/a_hub_of_all_time/main/scripts/wallwalk.lua",true))()
+   end,
+})
+--IY
+local Button = ScriptsTab:CreateButton({
+   Name = "Infinite Yield",
+   Callback = function()
+ 	loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source",true))()
    end,
 })
 
+--HUBS
+local HubsTab = Window:CreateTab("Hubs", 4483362458) -- Title, Image
+local Button = HubsTab:CreateButton({
+   Name = "DomainX",
+   Callback = function()
+ 	loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/DomainX/main/source',true))()
+   end,
+})
+local Button = HubsTab:CreateButton({
+   Name = "UniversalFE",
+   Callback = function()
+ 	loadstring(game:HttpGet('https://raw.githubusercontent.com/Dvrknvss/UniversalFEScriptHub/main/Script'))()
+   end,
+})
 
-local Label = Tab:CreateLabel("Other Hubs")
+--GAMES
+local GamesTab = Window:CreateTab("Games", 4483362458) -- Title, Image
+local Button = GamesTab:CreateButton({
+   Name = "Doors [TerminalVibes]",
+   Callback = function()
+ 	loadstring(game:HttpGet('https://raw.githubusercontent.com/OminousVibes-Exploit/Scripts/main/doors/loadstring.lua',true))()
+   end,
+})
 
-local Label = Tab:CreateLabel("Notes")
+local UtilTab = Window:CreateTab("Utility", 4483362458) -- Title, Image
+local Button = UtilTab:CreateButton({
+   Name = "Freecam",
+   Callback = function()
+ 	loadstring(game:HttpGet('https://pastebin.com/raw/yJ51HKy9',true))()
+   end,
+})
+--CAMERASWITCH
+local CameraTab = Window:CreateTab("Camera Switcher")
+local Label = CameraTab:CreateLabel("WARNING: THIS IS ALL IRRIVERSIBLE!!!")
+local Label = CameraTab:CreateLabel("RUN THESE AT YOUR OWN RISK!")
+--Auto Realistic Camera
+local Button = CameraTab:CreateButton({
+   Name = "Character Head Camera ",
+   Callback = function()
+ 	game:GetService("RunService").RenderStepped:Connect(function() workspace.CurrentCamera.CFrame = game.Players.LocalPlayer.Character.Head.CFrame end)
+   end,
+})
+
+local Paragraph = CameraTab:CreateParagraph({Title = "Customizable Camera", Content = "Do game.Workspace.WHATEVERPARTHERE.CFrame"})
+local textb = "okay"
+local Input = CameraTab:CreateInput({
+   Name = "Path",
+   PlaceholderText = "path",
+   RemoveTextAfterFocusLost = false,
+   Callback = function(Text)
+	game:GetService("RunService").RenderStepped:Connect(function() workspace.CurrentCamera.CFrame = tostring(Text..".CFrame") end)
+   end,
+})
+
+--CONTROLS
+local ControlTab = Window:CreateTab("Controls")
+local Paragraph = ControlTab:CreateParagraph({Title = "FeFlip", Content = "Z or X to flip, C to jump when in air."})
+local Paragraph = ControlTab:CreateParagraph({Title = "FreeCam", Content = "Shift + P to use."})
+local Paragraph = ControlTab:CreateParagraph({Title = "Doors Terminalvibes", Content = "RControl to use."})
